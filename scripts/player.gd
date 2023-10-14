635b35cfe11b54b9878f0ded9f3fb7d2ba731164
Samuel <sassani134@gmail.com>
2023-10-14 21:01:44 +0200

    visual rotat

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,6 +19,7 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal ))
+		visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal ))
 		camera_mount.rotate_x(deg_to_rad(- event.relative.y *  sens_vertical))
 
 func _physics_process(delta):
