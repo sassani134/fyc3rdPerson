435c4337070b0e70e4df8dbbac6aadba45fe2f13
Samuel <sassani134@gmail.com>
2024-01-19 03:38:27 +0100

    Adding 3d models ressource

--- scripts/player.gd
+++ scripts/player.gd
@@ -82,6 +82,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
